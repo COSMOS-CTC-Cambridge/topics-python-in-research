3a8ad8d7b7b266f6ec14b14bd7bd7afd4c996571
Elliot Lynch <eml52@rosalind.damtp.cam.ac.uk>
2017-01-04 17:16:28 +0000

    added file to generate data for pca and got normal pca working

--- sample-answers/principle_component_analysis.py
+++ sample-answers/principle_component_analysis.py
@@ -2,48 +2,67 @@ import numpy as np
 import scipy.linalg as lalg
 
  #ar1/2 is a 2D array of vectors
-def covarience_matrix(ar1,ar2):
-  
-  #far1 = np.fft.rfftn(ar1)
-  #far2 = np.fft.rfftn(ar2)
+def correllation(ar1,ar2):
 
-  Ncomponents = far1.shape[0]
-
-  #N1 = ar1.shape[0]
-  #N2 = ar2.shape[0]
+  Ncomponents = ar1.shape[0]
 
   CovMat = np.zeros((Ncomponents,Ncomponents))
 
   Nfreq = min(ar1.shape[1],ar2.shape[1])
 
-  far1 = np.fft.rfftn(ar1,axis=1,s=Nfreq)
-  far2 = np.fft.rfftn(ar2,axis=1,s=Nfreq)
-
   for i in xrange(Ncomponents):
     for j in xrange(Ncomponents):
-      fcoverience = far1[i]*far2[j]
-      CovMat[i,j] = np.sum(np.fft.irfftn(fcoverience,axis=1,s=Nfreq))
+      CovMat[i,j] = np.dot(ar1[i],ar1[j])
 
   return CovMat
 
 
-# ok abuse of terminology will be rife here.
-
-def autocorrelation(ar1):
-  return covarience_matrix(ar1,ar1)
+def covarience_matrix(ar1):
+  N = ar1.shape[0]
+  return correllation(ar1,ar1)/(N - 1)
 
 def PCA(ar1):
+  
 
-  covmat = autocorrelation(ar1)
+  print ar1.shape
+
+  nar1 = ar1 - np.outer(np.mean(ar1,axis=1),np.ones(ar1.shape[1]))
+
+  covmat = covarience_matrix(nar1)
  
-  eigval, eigvec = lalg.eig(ar1)
+  #print covmat
+
+  eigval, eigvec = lalg.eig(covmat)
 
   retar = np.zeros_like(ar1)
 
-  for i in xrange(eigval.size):  
-    retar[i] = np.dot(eigvec[i],ar1)
+  for i in xrange(ar1.shape[0]): 
+    retar[i] = np.dot(eigvec[:,i],nar1)
 
-  return retar
+  return retar, eigvec, eigval
+
+
+
+if __name__ == '__main__':
+  
+  import sys
+  import matplotlib.pylab as plt
+
+  filename = sys.argv[1]
+
+  data = np.loadtxt(filename,delimiter=',', skiprows=1)   
+
+  pca_data, eigvec, eigval = PCA(data) 
+ 
+  plt.plot(data[0],data[1], 'kx', label='initial')
+  plt.plot(pca_data[0],pca_data[1], 'bo', label='pca')
+  plt.legend(loc='best')
+  plt.show()
+
+
+
+
+
 
 
 
